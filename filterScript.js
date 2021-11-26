983bbac36e21fddbac5fb976b737a21700344aa7
Collin Lacoursiere <cml@Mac.local>
2021-11-25 21:31:55 -0500

    Adds filtering functionality - incomplete

---
+++ filterScript.js
@@ -0,0 +1,15 @@
+/* 
+Module to run filtering algorithms on the data. Will have a function that takes in all data, and all
+filtering args pulled in from the page's user input. Will run the algos over the data, and then call
+writeToPage (passed in from front-end module?) to write all the results back to the page.
+*/
+
+function runFiltering(args, data) {
+
+
+}
+
+
+function filterRated(data) {
+    
+}
